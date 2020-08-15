38ba17388adf5b6684e0087b2535832a602bceb1
NoahLiu <thumbb13555@gmail.com>
2020-08-15 19:03:12 +0800

    Initial commit

---
+++ app/src/main/java/com/jetec/switchdemo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.jetec.switchdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Switch swAbove = findViewById(R.id.switch_Above);
+        Switch swBelow = findViewById(R.id.switch_Below);
+        ToggleButton toggleButton = findViewById(R.id.toggleButton);
+
+        swAbove.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "Sw1: "+isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        swBelow.setOnCheckedChangeListener((buttonView,isChecked)->{
+            Toast.makeText(MainActivity.this, "Sw2: "+isChecked, Toast.LENGTH_SHORT).show();
+        });
+        toggleButton.setOnCheckedChangeListener((buttonView,isChecked)->{
+            Toast.makeText(MainActivity.this, "Tg1: "+isChecked, Toast.LENGTH_SHORT).show();
+        });
+    }
+}
